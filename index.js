d1e53127c5f96625bdfd0e28833ed28b8616938a
Soham Das <majumder70.ranu@gmail.com>
2021-11-13 20:42:14 +0530

    Creating options

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const prgm = require('commander');
-prgm.version('1.0.0').description('Password Generator')
-prgm.command('generate').action(() => console.log("Generated!!")).parse();
+prgm.version('1.0.0').description('Password Generator');
+
+prgm
+.option('-l, --length <number>', 'length of the password', '0')
+.option('-s, --save', 'save password to passwords.txt')
+.option('-nn, --no-number', 'remove numbers')
+.option('-ns, --no-symbol', 'remove symbols')
+.parse();
+
+const { length, save, number, symbol } = prgm.opts();
+console.log(length, number, symbol);
